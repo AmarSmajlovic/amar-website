542d7bdd3f945b0665bd25861d0dee1c9363c4f5
AmarSmajlovic <amarcsgo@gmail.com>
2022-09-17 16:30:36 +0200

    add styled components, navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import styled from "styled-components";
+import { Navbar } from "../components";
+import { GlobalStyle } from "../styles";
+
+const Container = styled.div`
+  color: red;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
 
 export default MyApp;
