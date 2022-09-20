e59cc39771724706355ef8fc0d48de988c339073
AmarSmajlovic <amarcsgo@gmail.com>
2022-09-20 16:01:37 +0200

    add skill component and skills page

---
+++ assets/SkillsIcons/index.ts
@@ -0,0 +1,23 @@
+import HtmlIcon from './HtmlIcon.svg';
+import CssIcon from './CssIcon.svg';
+import JavaScriptIcon from './JavaScriptIcon.svg';
+import TypescriptIcon from './TsIcon.svg';
+import ReactIcon from './ReactIcon.svg';
+import NextJSIcon from './NextJsIcon.svg';
+import FirebaseIcon from './FirebaseIcon.svg';
+import MySQLIcon from './MySQLIcon.svg';
+import NodeJSIcon from './NodeIcon.svg';
+import JestJSIcon from './JestIcon.svg';
+
+export {
+    HtmlIcon,
+    CssIcon,
+    JavaScriptIcon,
+    TypescriptIcon,
+    ReactIcon,
+    NextJSIcon,
+    FirebaseIcon,
+    MySQLIcon,
+    NodeJSIcon,
+    JestJSIcon
+}
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from './Navbar';
 export * from './SocialIcons';
 export * from './TypeWriter';
+export * from './Skill';
