055099d0f2bb9a2769620490d7526ea7d298c3a9
AmarSmajlovic <amar.smajlovic@ministryofprogramming.com>
2023-11-01 01:51:01 +0100

    Api for ip adress

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  ip?: string | string[];
+  message?: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const ip =
+      req.headers["x-forwarded-for"] ||
+      // Fallback for localhost or non Vercel deployments
+      "0.0.0.0";
+    res.status(200).json({ ip });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 }
